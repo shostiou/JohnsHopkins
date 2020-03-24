3de477e4c1e48b3b8fd0385f5dd5a5c4b118ac59
shostiou <shostiou@gmail.com>
2020-03-24 14:36:33 +0000

    JOHNS HOPKINS - MODULE 03 - Week 01

--- M03 - W01/JH_M03_W01.R
+++ M03 - W01/JH_M03_W01.R
@@ -34,4 +34,51 @@ colIndex <- 7:15
 dat <- read.xlsx("./data/NGAP.xlsx", sheetIndex=1, rowIndex = rowIndex, colIndex = colIndex)
 
 
+## Downloading BALTIMORE RESTAURANTS file from the Web
+## ------------------------------------------
+fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Frestaurants.xml"
+download.file(fileUrl, destfile="./data/restaurants.xml", method = "curl")
+
+## Reading downloaded data
+library(XML)
+doc <- xmlTreeParse(sub("s", "", fileUrl), useInternal = TRUE)
+rootNode <- xmlRoot(doc)
+xmlName(rootNode)
+
+## Getting access to ZipCode
+## rootNode[[1]][[1]][[2]]
+## <zipcode>21206</zipcode> 
+
+## Extracting zip code information
+zipcodelist <- xpathSApply(rootNode,"//zipcode",xmlValue)
+## Grouping results
+table(zipcodelist)
+
+
+
+## he American Community Survey distributes downloadable data about 
+## United States communities. Download the 2006 microdata survey about 
+## housing for the state of Idaho using download.file() from here: 
+## ---------------------------------------------------------------------
+fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06pid.csv"
+download.file(fileUrl, destfile="./data/micro.csv", method = "curl")
+
+# install.packages("data.table")
+library("data.table")
+DT=fread(fileUrl)
+
+## Tries to identify the fastest calculation 
+print(system.time(DT[,mean(pwgtp15),by=SEX]))
+print(system.time(sapply(split(DT$pwgtp15,DT$SEX),mean)))
+print(system.time(mean(DT$pwgtp15,by=DT$SEX)))
+print(system.time(tapply(DT$pwgtp15,DT$SEX,mean)))
+
+## SWirl
+install.packages("swirl")
+packageVersion("swirl")
+library(swirl)
+install_from_swirl("Getting and Cleaning Data")
+swirl()
+
+
 
