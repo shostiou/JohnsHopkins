d501ada789a04144d3a6c90aa12f4c5e309a40a8
shostiou <shostiou@gmail.com>
2020-03-27 16:21:05 +0000

    JOHNS HOPKINS - Coursera - Module 03 Getting Data - Update 2020/03/27

--- M03 - W02/JH_M03_W02.R
+++ M03 - W02/JH_M03_W02.R
@@ -11,7 +11,7 @@ if (!file.exists("data"))
 }
 
 
-## Getting data from the GitHub API
+## QUESTION 1 - Getting data from the GitHub API
 ## ------------------------------------------
 ## Register an application with the Github API here https://github.com/settings/applications. 
 ## Access the API to get information on your instructors repositories
@@ -49,4 +49,85 @@ stop_for_status(req)
 json1 = content(req)
 json2 = jsonlite::fromJSON(json1)
 json1[[16]]$created_at
+
+
+
+## QUESTION 2; 3 - sqldf package
+## ------------------------------------------
+## The sqldf package allows for execution of SQL commands on R data frames. 
+## We will use the sqldf package to practice the queries we might send with the dbSendQuery command in RMySQL.
+## Download the American Community Survey data and load it into an R object called 
+
+## Setting Working Directory
+setwd("/cloud/project/M03 - W02")
+
+## Preparing a directory to store data
+if (!file.exists("data"))
+{
+  dir.create("data")
+}
+
+## Downloading HOUSE PRICES file from the Web
+## ------------------------------------------
+fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06pid.csv"
+download.file(fileUrl, destfile="./data/survey.csv", method = "curl")
+
+## Recording download date
+dateDownloaded <- Date()
+
+## Reading downloaded data
+acs <- read.table("./data/survey.csv", sep =",", header=TRUE)
+
+## Installing the library
+## install.packages("sqldf")
+
+library(RMySQL)  ## This library has to be installed as well !
+library(sqldf)
+
+## Giving a try
+## It is necessary to specify the driver prior using it.
+options(sqldf.driver = "SQLite")
+sqldf("select pwgtp1 from acs where AGEP < 50")
+
+sqldf("select distinct AGEP from acs") == unique(acs$AGEP)
+
+
+## QUESTION 4 - Reading from HTML
+## ------------------------------------------
+## How many characters are in the 10th, 20th, 30th and 100th lines of HTML from this page:
+## http://biostat.jhsph.edu/~jleek/contact.html
+## (Hint: the nchar() function in R may be helpful)
+
+con <- url("http://biostat.jhsph.edu/~jleek/contact.html")
+htmlcode = readLines(con)
+close(con)
+
+nchar( htmlcode[10])
+nchar( htmlcode[20])
+nchar( htmlcode[30])
+nchar( htmlcode[100])
+
+
+
+## QUESTION 5 - 
+## Read this data set into R and report the sum of the numbers in the fourth of the nine columns.
+## https://d396qusza40orc.cloudfront.net/getdata%2Fwksst8110.for
+## (Hint this is a fixed width file format)
+
+## Setting Working Directory
+setwd("/cloud/project/M03 - W02")
+
+## Preparing a directory to store data
+if (!file.exists("data"))
+{
+  dir.create("data")
+}
+
+## ------------------------------------------
+fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fwksst8110.for"
+download.file(fileUrl, destfile="./data/indices.for", method = "curl")
+
+## A specific package exists to read fixed width file format
+x <- read.fwf(file="./data/indices.for",skip=4, widths=c(12, 7, 4, 9, 4, 9, 4, 9, 4))
+sum(x$V4)
 
