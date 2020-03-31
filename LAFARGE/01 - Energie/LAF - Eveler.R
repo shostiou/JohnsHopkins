5c68e0a9c78277baa3918a22c4f67aa3dc9c92e2
shostiou <shostiou@gmail.com>
2020-03-31 15:38:35 +0000

    Eveler - Poursuite des essais de connexion du 31/03/2020

--- LAFARGE/01 - Energie/LAF - Eveler.R
+++ LAFARGE/01 - Energie/LAF - Eveler.R
@@ -35,23 +35,46 @@ paste("Token reçu :", token_eveler)
 Meters_List <- GET("https://suite.eveler.pro/api/client/meters", add_headers(accept = "application/json",Authorization = token_eveler))
 Meters <- content(Meters_List, as = "parsed")
 
+# Unlist du resultat
+MyMeters <- lapply(Meters$data, function(x){ unlist(x)})
+# Conversion en dataframe
+MetersData <- do.call("rbind", MyMeters)
+MetersData <- as.data.frame(MetersData)
+
 # Lecture du nombre de compteurs
 Nb_Meters <- Meters$pagination$to
 
 # Preparation Dataframe pour recevoir liste des compteurs
-Meters_df <- data.frame(id=character(), 
-                        nom_site=character())
+Meters_df <- data.frame(client_id=character(), 
+                        nom_site=character(),
+                        rae=character())
 
 # Extraction de la liste des compteurs
 for (i in 1:Nb_Meters)
   {
       # Ligne de donnees
-      new_line <- data.frame(id=Meters$data[[i]]$'_id', 
-                             nom_site=Meters$data[[i]]$name)
+      new_line <- data.frame(client_id=Meters$data[[i]]$client_id, 
+                             nom_site=Meters$data[[i]]$name,
+                             rae=Meters$data[[i]]$rae)
       print(new_line)
       # Ajout de la ligne au DF
       Meters_df<- rbind(Meters_df,new_line)
   }
+
+
+library(xml2)
+
+# Collecte de la liste des index par compteur
+for (my_Meter in Meters_df$client_id[1])
+  {
+    # Collecte index du compteur
+    my_body<-paste0("https://suite.eveler.pro/api/client/meter",my_Meter)
+    Meters_Index <- GET(my_body, add_headers(accept = "application/json",Authorization = token_eveler))
+    My_Meters_Index <-content(Meters_Index, as = "parsed")
+    print(My_Meters_Index)
+  }
+
+
 
 
 ## FERMETURE DE LA CONNEXION
