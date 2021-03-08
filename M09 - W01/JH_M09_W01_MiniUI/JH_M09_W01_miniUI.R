77aefe8daea216a2ee0fb0f06c262c41bf487fad
shostiou <shostiou@gmail>
2021-03-08 11:57:41 +0100

    plotly assignment completed

--- M09 - W01/JH_M09_W01_MiniUI/JH_M09_W01_miniUI.R
+++ M09 - W01/JH_M09_W01_MiniUI/JH_M09_W01_miniUI.R
@@ -30,4 +30,52 @@ pickTrees <- function() {
   
  # Calling the gadget  
   runGadget(ui,server)
+}
+
+
+
+# Googlevis
+
+suppressPackageStartupMessages(library(googleVis))
+M <- gvisMotionChart(Fruits,"Fruit","Year",
+                     options = list(width=600,height=400))
+
+
+P <- gvisGeoChart(Exports,locationvar = "Country",
+                  colorvar = "Profit",
+                     options = list(width=600,height=400))
+print(P,"chart")
+
+
+
+
+# Quiz 1 Question 5
+
+library(shiny)
+library(miniUI)
+
+pickXY <- function() {
+  ui <- miniPage(
+    gadgetTitleBar("Select Points by Dragging your Mouse"),
+    miniContentPanel(
+      plotOutput("plot", height = "100%", brush = "brush")
+    )
+  )
+  
+  server <- function(input, output, session) {
+    output$plot <- renderPlot({
+      plot(data_frame$X, data_frame$Y, main = "Plot of Y versus X",
+           xlab = "X", ylab = "Y")
+    })
+    observeEvent(input$done, {
+      stopApp(brushedPoints(data_frame, input$brush,
+                            xvar = "X", yvar = "Y"))
+    })
   }
+  
+  runGadget(ui, server)
+}
+
+my_data <- data.frame(X = rnorm(100), Y = rnorm(100))
+
+pickXY(my_data)
