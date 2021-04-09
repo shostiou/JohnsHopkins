759ef6ab070e08f37439ac2c39bb6c727ef8ae27
shostiou <shostiou@gmail>
2021-04-09 19:01:14 +0200

    shiny app ready for publication

--- M10 - W01/wordprediction/ui.R
+++ M10 - W01/wordprediction/ui.R
@@ -13,15 +13,15 @@ library(R.utils)
 library(RSQLite)
 library(tictoc)
 library(stringr)
-# library(tm)
 library(dplyr)
 library(tidyr)
 
-# Define UI for application that draws a histogram
+# Define UI for application that invites the user to type text and read a prediction
+# of what could be the next word 
 shinyUI(fluidPage(
     
     # Application title
-    titlePanel("Next word prediction - Johns Hopkins Capstone"),
+    titlePanel("NEXT WORD PREDICTION - Shostiou's Johns Hopkins Capstone"),
     
     
     tabsetPanel(
@@ -29,34 +29,51 @@ shinyUI(fluidPage(
         tabPanel("Main", 
                  sidebarLayout(
                      sidebarPanel( 
-                         
-                         p(''),
-                         p('please select "Instructions" tab for help'),
-                         p(''),
-                         textInput("user_text", "Please type some text", value = "")
+                         h4("Predictions for the next word"),
+                         br(),
+                         br(),
+                         br(),
+                         textInput("user_text", "Please type some text in English", value = ""),
+                         p('Please refer to the "Instructions" tab for details'),
+                         br(),
+                         br(),
+                         br(),
+                         img(src='coursera_logo.png', align = "center"),
+                         br()
                      ), #sidebarPanel )
                      
-                     # Show a plot of the generated distribution
-                     
+                     # Displays predictions for the next word
                      mainPanel(
-                         h4("Next word prediction"),
+                         br(),
+                         br(),
+                         br(),
+                         br(),
+                         br(),
+                         br(),
                          tableOutput("result")
                      ) # mainPanel )
                      
                  )  #sidebarLayout )
         ), # tabPanel )
+        
         # instructions tab panel
         tabPanel("Instructions",
                  p(""),
-                 h2("User instructions"),
-                 p("This application is used to do a basic exploration of the trees data set."),
-                 p("It will automatically build a scatter plot between 2 variables of the data set"),
+                 h3("Instructions"),
+                 p("This application is used to predict the next word from a string typed by the user."),
+                 p("Please note that this program only builds prediction in the English language."),
+                 p("The utilization of the application is straight forward : just type some text and check for predictions of the next word."),
+                 p("This project concludes the Coursera Johns Hopkins' Data Science specilization."),
                  p(""),
-                 h3('Step by step approach : '),
-                 p("1. Please start by selecting the variable to be displayed on the x axis"),
-                 p("2. Please start by selecting the variable to be displayed on the y axis"),
-                 p("3. Please adjust the color of the plot according to your preferences"),
-                 p("4. Please adjust the size of the scatter plot points according to your preferences"),
+                 h3('Details : '),
+                 p("This program uses a combination of Markov chain rule & stupid backoff approaches "),
+                 p("The algorithm was trained on samples (20%) of a set of 3 large text files : us_news, us_blogs and us_twitter."),
+                 p("bi-grams, 3-grams and 4-grams have been built from the dataset."),
+                 p("Frequency of words have been computed out of those n-grams : P(word_n|value of n-1 words)."),
+                 p("This basically means : based on the last words typed by the user (4,3,2 last words), what is the probability to have the next word being word_n."),
+                 p("As an initial attempt, considering the last 3 words typed by the user, the program tries to figure out if there is a match in its reference database. "),
+                 p("If it exists, it returns 3 predictions for the next word by sorting results by highest probability."),
+                 p("If no match is found, the program tries to find entries corresponding to 3-grams and so on."),
         ) # tabPanel )
     ) # tabSetPanel )
     
